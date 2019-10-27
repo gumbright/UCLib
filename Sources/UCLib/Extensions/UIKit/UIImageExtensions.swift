97b4017421d31b2d73e2bece428c43fe9981945c
Guy Umbright <gumbright@gmail.com>
2019-10-26 21:38:30 -0500

    Add UIImage extension

---
+++ Sources/UCLib/Extensions/UIKit/UIImageExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageExtensions.swift
+//  UCLib
+//
+//  Created by Guy Umbright on 10/26/19.
+//
+
+import UIKit
+import CoreGraphics
+
+extension UIImage
+{
+    func resizeImage(_ image:UIImage, newSize:CGSize) -> UIImage
+    {
+        UIGraphicsBeginImageContext(newSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+}
