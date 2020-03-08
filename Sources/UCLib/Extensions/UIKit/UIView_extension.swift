e53b67d26051454275052a7788fc70ce65b78e2a
Guy Umbright <gumbright@gmail.com>
2020-03-07 18:52:57 -0600

    Add UIVIew extensions

---
+++ Sources/UCLib/Extensions/UIKit/UIView_extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIView_extension.swift
+//  tabletest
+//
+//  Created by Guy Umbright on 3/1/20.
+//  Copyright © 2020 Umbright Consutling, Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIView
+{
+    //let myCustomView: CustomView = UIView.fromNib()
+    //or
+    //let myCustomView: CustomView = .fromNib()
+    
+    class func fromNib<T: UIView>() -> T {
+        return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+}
