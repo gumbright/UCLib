989bf45afeaa2a39d212bc4b1a4f6b958e9ca514
Guy Umbright <gumbright@gmail.com>
2020-03-21 22:47:18 -0500

    Add NSManagedObjectExtensions

---
+++ Sources/UCLib/Extensions/CoreData/HWE_NSManagedObjectExtension.swift
@@ -0,0 +1,164 @@
+//
+//  NSManagedObjectExtension.swift
+//
+//  Created by Guy Umbright on 7/6/17.
+//  Copyright © 2017 Umbright Consulting, Inc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+// For many of these extension methods to work, an extension needs to be create for the NSManagedObject classes that
+// defines the entityName() method.  This allows things like <Classname>.allInContext(...)
+
+public extension NSManagedObject
+{
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    class func allInContext<T:NSFetchRequestResult>(context:NSManagedObjectContext,predicate:NSPredicate?,sortedBy:String?,ascending:Bool = false) -> [T]
+    {
+        let request = NSFetchRequest<T>(entityName: self.entityName())
+        request.predicate = predicate
+        if let sortField = sortedBy
+        {
+            let sortDesc = NSSortDescriptor(key: sortField, ascending: ascending)
+            request.sortDescriptors = [sortDesc]
+        }
+        
+        var result : [T] = Array<T>()
+        context.performAndWait {
+            do {
+                result = try context.fetch(request)
+            }
+            catch {}
+        }
+        
+        return result
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    class func firstInContext<T:NSManagedObject>(context:NSManagedObjectContext,predicate:NSPredicate?,sortedBy:String?,ascending:Bool = false) -> T?
+    {
+        let result:[T] = self.allInContext(context: context, predicate: predicate, sortedBy: sortedBy, ascending:ascending)
+        if result.count > 0
+        {
+            return result[0];
+        }
+        else
+        {
+            return nil
+        }
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    class func countWith(predicate:NSPredicate?, context:NSManagedObjectContext) -> Int
+    {
+        let request = NSFetchRequest<NSManagedObject>(entityName: self.entityName())
+        var result = 0
+        
+        request.predicate = predicate
+        context.performAndWait {
+            do
+            {
+                result = try context.count(for: request)
+            }
+            catch
+            {
+                
+            }
+        }
+        return result
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    class func create<T:NSManagedObject>(context:NSManagedObjectContext) -> T?
+    {
+        if let entity = self.entityDescription(context: context)
+        {
+            return T(entity: entity, insertInto: context)
+        }
+        else
+        {
+            return nil
+        }
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    class func create<T:NSManagedObject>(context:NSManagedObjectContext, objectName:String) -> T?
+    {
+        //if let entity = self.HWE_entityDescription(context: context)
+        if let entity = NSEntityDescription.entity(forEntityName: objectName, in: context)
+        {
+            return T(entity: entity, insertInto: context)
+        }
+        else
+        {
+            return nil
+        }
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    class func entityDescription(context:NSManagedObjectContext) -> NSEntityDescription?
+    {
+        return NSEntityDescription.entity(forEntityName: self.entityName(), in: context)
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    class func entityName() -> String
+    {
+        return String(describing:Self.self)
+//        var entityName = self.entityName()
+//        
+//        if entityName == nil
+//        {
+//            entityName = NSStringFromClass(self).components(separatedBy: ".").last!
+//        }
+//        
+//        return entityName!
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    @objc class func entityName() -> String?
+    {
+        return nil
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func delete()
+    {
+        self.deleteFromContext(context:self.managedObjectContext!)
+    }
+    
+    /////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////
+    func deleteFromContext(context:NSManagedObjectContext)
+    {
+        do {
+            let objInContext = try context.existingObject(with: self.objectID)
+            context.delete(objInContext)
+        }
+        catch
+        {
+            //do nothing for now
+        }
+        
+    }
+ }
