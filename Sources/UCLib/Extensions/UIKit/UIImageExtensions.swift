6c1da83b4a5b1197925054af5065a72ddffa1008
Guy Umbright <gumbright@gmail.com>
2020-11-25 20:05:15 -0600

    add new resize method, now requires iOS 10.x

--- Sources/UCLib/Extensions/UIKit/UIImageExtensions.swift
+++ Sources/UCLib/Extensions/UIKit/UIImageExtensions.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreGraphics
 
+@available(iOS 10.0, *)
 public extension UIImage
 {
     func resizeTo(_ newSize:CGSize) -> UIImage
@@ -17,5 +18,52 @@ public extension UIImage
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return newImage!
+    }
+    
+    //scale image to desired size (
+    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
+        // Determine the scale factor that preserves aspect ratio
+        let widthRatio = targetSize.width / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        let scaleFactor = min(widthRatio, heightRatio)
+        
+        // Compute the new image size that preserves aspect ratio
+        let scaledImageSize = CGSize(
+            width: size.width * scaleFactor,
+            height: size.height * scaleFactor
+        )
+
+        // Draw and return the resized UIImage
+        let renderer = UIGraphicsImageRenderer(
+            size: scaledImageSize
+        )
+
+        let scaledImage = renderer.image { _ in
+            self.draw(in: CGRect(
+                origin: .zero,
+                size: scaledImageSize
+            ))
+        }
+        
+        return scaledImage
+    }
+    
+    //just draw it into the box I say
+    func resizeInto(_ newSize : CGSize) -> UIImage
+    {
+        let renderer = UIGraphicsImageRenderer(
+            size: newSize
+        )
+
+        let scaledImage = renderer.image { _ in
+            self.draw(in: CGRect(
+                origin: .zero,
+                size: newSize
+            ))
+        }
+        
+        return scaledImage
+
     }
 }
