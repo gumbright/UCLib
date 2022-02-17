3142d6f8f023ee7dd9ec8a5fc7e847c17a18755e
Guy Umbright <gumbright@gmail.com>
2022-02-17 12:08:38 -0600

    add platforms to package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "UCLib",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
